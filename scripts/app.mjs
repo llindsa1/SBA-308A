6a86c20619dfdca6cab62d8b2989e939630061f1
llindsa1 <lindsayuchiha@icloud.com>
2024-10-07 10:17:12 -0400

    6

---
+++ scripts/app.mjs
@@ -0,0 +1,3 @@
+import { fetchBreeds, fetchImagesByBreed } from "./api.mjs";
+import { renderBreedsDropdown, renderImageGallery, showNotification } from "./ui.mjs";
+
--- scripts/ui.mjs
+++ scripts/ui.mjs
@@ -6,3 +6,23 @@ export const renderBreedsDropdown = (breeds) => {
     `).join('');
 };
 
+//Image gallery
+export const renderImageGallery = (images) => {
+    const galleryElement = document.getElementById('gallery');
+    galleryElement.innerHTML = images.map(image => `
+        <div class="gallery-item">
+        <img src="${image.url}" alt="Dog Image">
+        <button data-id="${image.id}" class="save-favorite-btn">Save as Favorite</button>
+        </div>
+        `).join('');
+};
+
+//Notifications
+export const showNotification = (message) => {
+    const notificationElement = document.getElementById('notification');
+    notificationElement.textContent = message;
+    notificationElement.style.display = 'block';
+    setTimeout (() => {
+        notificationElement.style.display = 'none';
+    }, 3000);
+};
