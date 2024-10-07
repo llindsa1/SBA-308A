1381867e029b9dab32b48f27a8d0b33d2da9f717
llindsa1 <lindsayuchiha@icloud.com>
2024-10-07 11:49:48 -0400

    final attempt

--- scripts/api.mjs
+++ scripts/api.mjs
@@ -34,3 +34,21 @@ export const fetchImagesByBreed = async (breed_id,limit=9) => {
     }
 };
 
+import { fetchBreeds, fetchImagesByBreed } from "./api.mjs";
+import { renderBreedsDropdown, renderImageGallery, showNotification } from "./ui.mjs";
+const initApp = async () => {
+    const breeds = await fetchBreeds();
+    renderBreedsDropdown(breeds);
+
+const breedSelect = document.getElementById('breed-select');
+breedSelect.addEventListener('change', async (event) => {
+    const breed_id = event.target.value;
+    const images = await fetchImagesByBreed(breed_id);
+    renderImageGallery(images);
+});
+};
+
+window.onload = () => {
+    initApp();
+};
+
